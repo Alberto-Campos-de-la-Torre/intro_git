e20f56278a689a687ee651cfb1aa242f8397f368
Alberto Campos de la Torre <alberto.campos3077@alumnos.udg.mx>
2020-09-14 14:39:02 -0500

    Capturamos la edad e imprimimos la edad

--- main.c
+++ main.c
@@ -2,12 +2,15 @@
 
 int main(){
     char nombre[100];
+    unsigned int edad;
 
     printf("Hola Mundo!\n");
     printf("Escribe tu nombre\n");
     fgets(nombre,sizeof(nombre),stdin);
     printf("Hola %s",nombre);
-
+    printf("Edad:\n");
+    scanf("%u",&edad);
+    printf("Vas a cumplir=%i\n",edad+1);
     return 0;
 }
 
